5e590d901f4c0286f32d29de347bf0666ed89802
Table 4 <touloniut@gmail.com>
2023-11-15 12:29:58 +0100

    csharp15_11
    page 13 !!!
    bouton test et tableau validé, voir pq les données du tableau sont inexploitables en comparant les valeurs sur l'interface et cellles sur le bus (à laide de l'oscilloscope)  (q tester l'applications).

--- c_sharp/InterfaceRobot/MainWindow.xaml.cs
+++ c_sharp/InterfaceRobot/MainWindow.xaml.cs
@@ -14,31 +14,47 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using ExtendedSerialPort;
+using System.Windows.Threading;
 
 namespace InterfaceRobot
 {
     /// <summary>
     /// Logique d'interaction pour MainWindow.xaml
     /// </summary>
+
+    
     
     public partial class MainWindow : Window
     {
         ReliableSerialPort serialPort1;
+        DispatcherTimer timerAffichage;
 
+        Robot robot = new Robot();
         public MainWindow()
         {
             InitializeComponent();
-            serialPort1 = new ReliableSerialPort("COM4", 115200, Parity.None, 8, StopBits.One);
+            serialPort1 = new ReliableSerialPort("COM6", 115200, Parity.None, 8, StopBits.One);
             serialPort1.OnDataReceivedEvent += SerialPort1_OnDataReceivedEvent;
             serialPort1.Open();
-            
+            timerAffichage = new DispatcherTimer();
+            timerAffichage.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            timerAffichage.Tick += TimerAffichage_Tick; ;
+            timerAffichage.Start();
 
         }
 
-        String receivedText ;
+        private void TimerAffichage_Tick(object sender, EventArgs e)
+        {
+            if(robot.receivedText != "" && robot.receivedText != "\r")
+            {
+                textBoxReception.Text += robot.receivedText;
+                robot.receivedText = "";
+            }
+        }
+
         public void SerialPort1_OnDataReceivedEvent(object sender, DataReceivedArgs e)
         {
-            receivedText += Encoding.UTF8.GetString(e.Data, 0, e.Data.Length);
+            robot.receivedText += Encoding.UTF8.GetString(e.Data, 0, e.Data.Length);
         }
 
         bool toggle = true ;
@@ -55,8 +71,51 @@ namespace InterfaceRobot
                 buttonEnvoyer.Background = Brushes.Beige;
                 toggle = true;
             }
-            textBoxReception.Text +="Reçu : "+ textBoxEmission.Text +"\n";
-            textBoxEmission.Text = "";
+            SendMessage();
+            //textBoxReception.Text +="Reçu : "+ textBoxEmission.Text +"\n";
+            textBoxEmission.Clear();
+        }
+
+        bool clear = true;
+
+        private void buttonClear_Click(object sender, RoutedEventArgs e)
+        {
+            if (clear)
+            {
+                buttonClear.Background = Brushes.RoyalBlue;
+                clear = false;
+            }
+            else
+            {
+                buttonClear.Background = Brushes.Beige;
+                clear = true;
+            }
+            textBoxReception.Clear();
+        }
+
+        bool test = true;
+
+        private void buttonTest_Click(object sender, RoutedEventArgs e)
+        {
+            if (test)
+            {
+                buttonClear.Background = Brushes.RoyalBlue;
+                test = false;
+            }
+            else
+            {
+                buttonClear.Background = Brushes.Beige;
+                test = true;
+            }
+
+            byte[] byteList;
+            byteList = new byte[20];
+
+            for(int i=0; i<20; i++)
+            {
+                byteList[i] = (byte)(2 * i);
+                serialPort1.WriteLine(byteList[i].ToString());
+            }
         }
 
 
@@ -82,8 +141,9 @@ namespace InterfaceRobot
             if (e.Key == Key.Enter)
             {
                 SendMessage() ;
+                textBoxEmission.Clear();
             }
-
+            
         }
     }
 }
